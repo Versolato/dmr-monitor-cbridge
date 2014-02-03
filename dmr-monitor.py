6270ca43cdf6675efe868a8e73917e51c8a39a47
Cort Buffington <n0mjs@me.com>
2014-02-02 20:13:06 -0600

    Added syslog for debugging

--- dmr-monitor.py
+++ dmr-monitor.py
@@ -13,6 +13,7 @@ import signal
 import sys
 import os
 import socket
+import syslog
 
 #-----------------------------
 # USER CONFIGURATION
@@ -29,8 +30,11 @@ DMR_PORT_RANGE = '50000-60000'
 TCPDUMP = ''
 PID = ''
 
-# Function to be called if we recieve a termination signal - we have to clea up the child before exit
+# Function to be called if we recieve a termination signal - we have to clean up the child before exit
 def handler(_signal, _frame):
+    message = 'Terminating proccesses with signal: ' + str( _signal)
+    syslog.syslog(syslog.LOG_CRIT, message)
+    syslog.closelog()
     os.kill(PID, 15)
     print 'Terminating child proccess with signal:', PID, _signal
     print 'Terminating main process with signal', _signal
@@ -42,6 +46,8 @@ def start_tcpdump():
     global TCPDUMP, PID
     TCPDUMP = subprocess.Popen(['/usr/local/bin/modtcpdump','-x','-l','-n','-Tdmr','-i','eth0','udp','portrange',DMR_PORT_RANGE,'and','host',LOCAL_IP], stdout=subprocess.PIPE)
     PID = TCPDUMP.pid
+    message = 'tcpdump started with PID: ' + str(PID)
+    syslog.syslog(syslog.LOG_INFO, message)
 
 
 # Function to keep seding data from the subprocess as long as it's alive
@@ -49,9 +55,15 @@ def send_data():
     while TCPDUMP.poll() != 0:
         line = TCPDUMP.stdout.readline()
         my_socket.sendto(line, (DEST_IP, DEST_PORT))
+    message = 'tcpdump has terminated. PID: ' + str(PID)
+    syslog.syslog(syslog.LOG_ERR, message)
 
 
 if __name__ == '__main__':
+
+    syslog.openlog('DMR-Monitor')
+    message = 'Program Invoked as: ' + sys.argv[0]
+    syslog.syslog(syslog.LOG_INFO, message)
 
     # Set signal handers so that we can gracefully exit if need be
     for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
